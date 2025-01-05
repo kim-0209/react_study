e4e08567d64ef711c7417a803ed0658aa831d27c
kimseungjin <ktk5594@naver.com>
2025-01-05 23:08:01 +0900

    [add] 2025/01/05 리액트

---
+++ section09/src/components/List.jsx
@@ -0,0 +1,17 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+
+const List = () => {
+  return (
+    <div className="List">
+      <h4>Todo List📌</h4>
+      <input type="text" placeholder="검색어를 입력하세요" />
+      <div className="todos_wrapper">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+    </div>
+  );
+};
+export default List;
