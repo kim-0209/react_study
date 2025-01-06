279c7001f3aaf2c92e3201d1677b29ae0ce4ccea
kimseungjin <ktk5594@naver.com>
2025-01-06 21:44:00 +0900

    [add]2025/01/06 마무리

--- section09/src/components/Editor.jsx
+++ section09/src/components/Editor.jsx
@@ -1,10 +1,39 @@
 import "./Editor.css";
+import { useState, useRef } from "react";
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (!content) {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent();
+  };
+
   return (
     <div className="Editor">
-      <input placeholder="새로우 Todo..." />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown}
+        placeholder="새로우 Todo..."
+        onChange={onChangeContent}
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 };
